b055755d77200d67215db1b3ca17d1cbe6588b41
JinToliq <a.homeliev@gmail.com>
2024-07-05 23:27:38 +0200

    V 1.0

---
+++ Runtime/TargetTweener.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace JinToliq.SimpleTween
+{
+  public abstract class TargetTweener<T> : Tweener where T : Component
+  {
+    public T Target;
+
+    private void Awake()
+    {
+      Target ??= GetComponent<T>();
+    }
+  }
+}
